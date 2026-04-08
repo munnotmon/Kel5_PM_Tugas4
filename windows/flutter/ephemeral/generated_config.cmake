51ca8810cd20fda12cb00b5ba761a6fe346241a7
fernandaay7 <fernandaayuputri07>
2026-04-08 23:20:25 +0700

    perubahan

---
+++ windows/flutter/ephemeral/generated_config.cmake
@@ -0,0 +1,24 @@
+# Generated code do not commit.
+file(TO_CMAKE_PATH "C:\\Android\\flutter" FLUTTER_ROOT)
+file(TO_CMAKE_PATH "C:\\Users\\Ayu\\pemrogramanmobile\\tugaspm" PROJECT_DIR)
+
+set(FLUTTER_VERSION "1.0.0+1" PARENT_SCOPE)
+set(FLUTTER_VERSION_MAJOR 1 PARENT_SCOPE)
+set(FLUTTER_VERSION_MINOR 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_PATCH 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_BUILD 1 PARENT_SCOPE)
+
+# Environment variables to pass to tool_backend.sh
+list(APPEND FLUTTER_TOOL_ENVIRONMENT
+  "FLUTTER_ROOT=C:\\Android\\flutter"
+  "PROJECT_DIR=C:\\Users\\Ayu\\pemrogramanmobile\\tugaspm"
+  "FLUTTER_ROOT=C:\\Android\\flutter"
+  "FLUTTER_EPHEMERAL_DIR=C:\\Users\\Ayu\\pemrogramanmobile\\tugaspm\\windows\\flutter\\ephemeral"
+  "PROJECT_DIR=C:\\Users\\Ayu\\pemrogramanmobile\\tugaspm"
+  "FLUTTER_TARGET=C:\\Users\\Ayu\\pemrogramanmobile\\tugaspm\\lib\\main.dart"
+  "DART_DEFINES=RkxVVFRFUl9WRVJTSU9OPTMuNDEuMQ==,RkxVVFRFUl9DSEFOTkVMPXN0YWJsZQ==,RkxVVFRFUl9HSVRfVVJMPWh0dHBzOi8vZ2l0aHViLmNvbS9mbHV0dGVyL2ZsdXR0ZXIuZ2l0,RkxVVFRFUl9GUkFNRVdPUktfUkVWSVNJT049NTgyYTBlN2M1NQ==,RkxVVFRFUl9FTkdJTkVfUkVWSVNJT049MzQ1MmQ3MzViZA==,RkxVVFRFUl9EQVJUX1ZFUlNJT049My4xMS4w"
+  "DART_OBFUSCATION=false"
+  "TRACK_WIDGET_CREATION=true"
+  "TREE_SHAKE_ICONS=false"
+  "PACKAGE_CONFIG=C:\\Users\\Ayu\\pemrogramanmobile\\tugaspm\\.dart_tool\\package_config.json"
+)
